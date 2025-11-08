551bdf02600c4050f06d12a3ed8097100bc93b44
Natdanai <natda002@gmail.com>
2025-11-08 18:43:01 +0700

    show image

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useState} from "react";
+import Image from "next/image";
+
 
 export default function Home() {
   const [books, setBooks] = useState([
@@ -17,6 +19,7 @@ export default function Home() {
   return (
     <div className="p-4">
       <button className="button">My Button</button>
+      <Image src='/next.svg' width={200} height={200} alt="Vercel Logo" className="mb-3"></Image>
       {books.map(item => (
         <div key={item.id}>{item.id}: {item.name}</div>
       ))}
